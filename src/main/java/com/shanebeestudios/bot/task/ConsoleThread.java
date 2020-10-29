3d52d9109fe699aad5af7b9f0b062c80940f1e25
Shane Bee <shanebolenback@me.com>
2020-10-29 02:08:13 -0700

    ConsoleThread - updated commands - included help and version command

--- src/main/java/com/shanebeestudios/bot/task/ConsoleThread.java
+++ src/main/java/com/shanebeestudios/bot/task/ConsoleThread.java
@@ -2,12 +2,26 @@ package com.shanebeestudios.bot.task;
 
 import com.shanebeestudios.bot.util.Logger;
 
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class ConsoleThread extends Thread {
 
+    private final String version;
+
     public ConsoleThread(String name) {
         super(name);
+        String version1 = "<unknown>";
+        Properties properties = new Properties();
+        try {
+            properties.load(this.getContextClassLoader().getResourceAsStream("properties.properties"));
+            version1 = properties.getProperty("version");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        version = version1;
     }
 
     @Override
@@ -15,9 +29,21 @@ public class ConsoleThread extends Thread {
         Scanner sc = new Scanner(System.in);
 
         while(sc.hasNextLine()) {
-            if (sc.next().equalsIgnoreCase("stop")) {
-                Logger.info("Stopping server!");
-                System.exit(0);
+            String message = sc.next();
+            switch (message.toLowerCase(Locale.ROOT)) {
+                case "help":
+                    Logger.info("Available commands:<blue> stop, version");
+                    break;
+                case "stop":
+                    Logger.info("Stopping server!");
+                    System.exit(0);
+                    break;
+                case "version":
+                case "ver":
+                    Logger.info("Running ShaneBot version: " + version);
+                    break;
+                default:
+                    Logger.warn("Unknown command '%s', use 'help' for available commands", message);
             }
         }
     }
