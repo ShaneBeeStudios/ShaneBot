605db5e9f2daa8c5c45de48d454eee7a8b97e076
ShaneBee <shanebolenback@me.com>
2024-04-17 16:54:00 -0700

    CommandSay - cleanup

--- src/main/java/com/shanebeestudios/bot/command/CommandSay.java
+++ src/main/java/com/shanebeestudios/bot/command/CommandSay.java
@@ -3,20 +3,17 @@ package com.shanebeestudios.bot.command;
 import com.shanebeestudios.bot.BotHandler;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-
-import java.util.EnumSet;
 
 public class CommandSay extends CommandBase {
 
     public CommandSay(BotHandler botHandler, Permission permission) {
         super(botHandler, permission, "Say", "Make the bot send a message");
         botHandler.getGuild().upsertCommand(getCommandName(), getDescription())
-                .addOption(OptionType.STRING, "message", "Message to send", true)
-                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(getPermission()))
-                .queue();
+            .addOption(OptionType.STRING, "message", "Message to send", true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(getPermission()))
+            .queue();
     }
 
     @SuppressWarnings("DataFlowIssue")
