2e7fe3b89b80da7070a0e2f0d90d060dc36e0e94
ShaneBee <shanebolenback@me.com>
2024-04-17 18:43:50 -0700

    CommandRelease - cleanup

--- src/main/java/com/shanebeestudios/bot/command/CommandRelease.java
+++ src/main/java/com/shanebeestudios/bot/command/CommandRelease.java
@@ -11,7 +11,6 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 import java.awt.*;
 import java.util.Date;
-import java.util.EnumSet;
 import java.util.Random;
 
 public class CommandRelease extends CommandBase {
@@ -19,19 +18,19 @@ public class CommandRelease extends CommandBase {
     public CommandRelease(BotHandler botHandler, Permission permission) {
         super(botHandler, permission, "Release", "Publish a release");
         botHandler.getGuild().upsertCommand(getCommandName(), getDescription())
-                .addOption(OptionType.STRING, "plugin", "Name of plugin", true)
-                .addOption(OptionType.STRING, "version", "Version of release", true)
-                .addOption(OptionType.STRING, "link", "link for release", true)
-                .addOption(OptionType.STRING, "description", "Description of release", true)
-                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(getPermission()))
-                .queue();
+            .addOption(OptionType.STRING, "plugin", "Name of plugin", true)
+            .addOption(OptionType.STRING, "version", "Version of release", true)
+            .addOption(OptionType.STRING, "link", "link for release", true)
+            .addOption(OptionType.STRING, "description", "Description of release", true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(getPermission()))
+            .queue();
     }
 
     @SuppressWarnings("DataFlowIssue")
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (!event.getName().equalsIgnoreCase("release")) return;
-        event.deferReply().queue();
+        event.deferReply().setEphemeral(true).queue();
 
         String plugin = event.getOption("plugin").getAsString();
         String version = event.getOption("version").getAsString();
@@ -42,14 +41,14 @@ public class CommandRelease extends CommandBase {
         Color color = new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
 
         MessageEmbed pluginRelease = new EmbedBuilder()
-                .setColor(color)
-                .setThumbnail(Util.IMAGE_URL)
-                .setAuthor("Plugin Release", null, Util.IMAGE_URL)
-                .setTitle("**" + plugin + " - " + version + "**")
-                .setDescription(desc)
-                .addField("**Download:**", link, false)
-                .setFooter(this.botHandler.getBotName(), this.botHandler.getBot().getSelfUser().getAvatarUrl())
-                .setTimestamp(new Date(System.currentTimeMillis()).toInstant()).build();
+            .setColor(color)
+            .setThumbnail(Util.IMAGE_URL)
+            .setAuthor("Plugin Release", null, Util.IMAGE_URL)
+            .setTitle("**" + plugin + " - " + version + "**")
+            .setDescription(desc)
+            .addField("**Download:**", link, false)
+            .setFooter(this.botHandler.getBotName(), this.botHandler.getBot().getSelfUser().getAvatarUrl())
+            .setTimestamp(new Date(System.currentTimeMillis()).toInstant()).build();
 
         event.getChannel().sendMessageEmbeds(pluginRelease).complete();
         event.getHook().deleteOriginal().queue();
