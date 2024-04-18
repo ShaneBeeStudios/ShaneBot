0ddad46ba46b90b20540a6d706feb74e7376c214
ShaneBee <shanebolenback@me.com>
2024-04-17 18:02:21 -0700

    CommandMute - cleanup

--- src/main/java/com/shanebeestudios/bot/command/CommandMute.java
+++ src/main/java/com/shanebeestudios/bot/command/CommandMute.java
@@ -3,36 +3,37 @@ package com.shanebeestudios.bot.command;
 import com.shanebeestudios.bot.BotHandler;
 import com.shanebeestudios.bot.util.MemberUtil;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class CommandMute extends CommandBase {
     public CommandMute(BotHandler botHandler, Permission permission) {
         super(botHandler, permission, "Mute", "Mute a user with a message sent to them and the bot channel.");
         botHandler.getGuild().upsertCommand(getCommandName(), getDescription())
-                .addOption(OptionType.USER, "member", "Member to mute", true)
-                .addOption(OptionType.INTEGER, "time", "Time something something", true)
-                .addOptions(new OptionData(OptionType.INTEGER, "timeunit", "Unit of time measurement", true)
-                        .addChoice("minutes", 0)
-                        .addChoice("hours", 1)
-                        .addChoice("days", 2))
-                .addOption(OptionType.STRING, "reason", "Reason for mute", true)
-                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(getPermission()))
-                .queue();
+            .addOption(OptionType.USER, "member", "Member to mute", true)
+            .addOption(OptionType.INTEGER, "time", "Amount of time", true)
+            .addOptions(new OptionData(OptionType.INTEGER, "timeunit", "Unit of time measurement", true)
+                .addChoice("minutes", 0)
+                .addChoice("hours", 1)
+                .addChoice("days", 2))
+            .addOption(OptionType.STRING, "reason", "Reason for mute", true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(getPermission()))
+            .queue();
     }
 
     @SuppressWarnings("DataFlowIssue")
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (!event.getName().equalsIgnoreCase(getCommandName())) return;
-        event.deferReply().queue();
+        event.deferReply().setEphemeral(true).queue();
 
-        Member member = event.getOption("member").getAsMember();
+        User user = event.getOption("member").getAsUser();
         int time = event.getOption("time").getAsInt();
         int timeU = event.getOption("timeunit").getAsInt();
         String reason = event.getOption("reason").getAsString();
@@ -43,8 +44,11 @@ public class CommandMute extends CommandBase {
             default -> TimeUnit.MINUTES;
         };
 
-        MemberUtil.timeoutMember(member, time, timeUnit, reason, event.getMember());
-        event.getHook().deleteOriginal().queue();
+        event.getGuild().retrieveMemberById(user.getId()).queue(member -> {
+            MemberUtil.timeoutMember(member, time, timeUnit, reason, event.getMember());
+            event.getHook().editOriginal("User **" + member.getEffectiveName() + "** muted for **" + time + " " +
+                timeUnit.toString().toLowerCase(Locale.ROOT) + "** due to **\"" + reason + "\"**!").queue();
+        }, fail -> event.getHook().editOriginal("Invalid User **" + user.getEffectiveName() + "**").queue());
     }
 
 }
